9f594ef472a1b692796b8cbd32c7f290393b3a56
ysunnatov <141179258+ysunnatov@users.noreply.github.com>
2023-08-20 16:11:15 +0500

    Delete app-restart.ps1

--- app-restart.ps1
+++
@@ -1,8 +0,0 @@
-$pathToApp = 'C:\inetpub\wwwroot\insurance'
-
-
-New-Item -Path $pathToApp -Name "app_offline.htm" -ItemType "file"
-
-# Provide script commands here to deploy the app
-
-Remove-Item -Path $pathToApp\app_offline.htm
