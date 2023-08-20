0567165d25da423ae057416e535ea2532e7bca88
ysunnatov <141179258+ysunnatov@users.noreply.github.com>
2023-08-20 21:42:33 +0500

    Delete app-restart.ps1

--- app-restart.ps1
+++
@@ -1,8 +0,0 @@
-$pathToApp = 'C:\dev\tools\actions-runner\projects'
-
-
-New-Item -Path $pathToApp -Name "app_offline.htm" -ItemType "file"
-
-# Provide script commands here to deploy the app
-
-Remove-Item -Path $pathToApp\app_offline.htm
