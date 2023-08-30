a243b87018881b77a76dfb4b4d6a6bb44aadefb1
ysunnatov <141179258+ysunnatov@users.noreply.github.com>
2023-08-30 10:51:34 +0500

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -23,8 +23,8 @@ param
 
 )
 
-    github.head_ref = ${GITHUB_REF#refs/heads/}
-    github.sha = $(git rev-parse --short "$GITHUB_SHA") 
+    $github.head_ref = ${GITHUB_REF#refs/heads/}
+    $github.sha = $(git rev-parse --short "$GITHUB_SHA") 
     
 $ErrorActionPreference = "stop"
 
